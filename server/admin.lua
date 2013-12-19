fca7226d7e8c3944d2d225bffcb0bcd16370068b
CainFoool <cain.donaghey@live.co.uk>
2013-12-19 07:40:27 +0000

    Fixed var issue, should solve the error in console

--- server/admin.lua
+++ server/admin.lua
@@ -64,7 +64,7 @@ function admin:loadAdmins(filename)
 	for line in file:lines() do
 		i = i + 1
 		
-		if string.sub(text, 1, 2) ~= "--" then
+		if string.sub(filename, 1, 2) ~= "--" then
 			admins[i] = line
 			print("Admins Found: " .. line)
 		end
