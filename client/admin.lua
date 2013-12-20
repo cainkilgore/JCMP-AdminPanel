a1e2979237c2d467c420ffb67e3ce52c955af49d
CainFoool <cain.donaghey@live.co.uk>
2013-12-20 02:35:23 +0000

    v0.0.0.7 Changelog
    ○ Added * support for /ptphere
    ○ Increased how message handling worked
    ○ Added /mass <value> command
    ○ Added /remveh command
    ○ Added /settime command
    ○ Added /weather command
    ○ Added /down command
    ○ Added /clear command
    ○ Added /pinkmobile command
    ○ Added check to see if you're teleporting to yourself
    ○ Fixed /online not showing online player names
    ○ Fixed /repair not cleaning your vehicle

--- client/admin.lua
+++ client/admin.lua
@@ -25,3 +25,31 @@ ClientFunction = function(sentMessage)
 end
 -- Subscribe ClientFunction to the network event "Test".
 Network:Subscribe("Test", ClientFunction)
+
+function ModulesLoad()
+	Events:FireRegisteredEvent( "HelpAddItem",
+        {
+            name = "Cain's Admin",
+            text = 
+                "This script gives Administrators/Players various uses of gameplay mechanics.\n\n" ..
+                "User Commands: /kill, /repair, /killcar, /id, /ptp <player>, /online, /sky\n" ..
+                "User Commands: /clear, /pinkmobile\n\n" ..
+                "Admin Commands: /kill <player>, /kick <player>, /setmoney <player> <amount>\n" ..
+				"Admin Commands: /forcepassenger <player>, /ptphere */<player>\n" ..
+				"Admin Commands: /ban <player>, /addmoney */<player> <amount>, /clear <player>,\n" ..
+				"Admin Commands: /notice <message>, /remveh, /mass <mass>, /settime <time>, /weather <value>\n\n" ..
+				"This Script was created by Cain from jc-mp.com.\nCurrent Version: v0.0.0.7\n\n" ..
+				"If you want to add more administrators, open admins.txt and insert Steam ID's." ..
+				"\n\nVisit the official script's server @ jc-mp.co.uk (IP and Website)"
+        } )
+end
+
+function ModuleUnload()
+    Events:FireRegisteredEvent( "HelpRemoveItem",
+        {
+            name = "Cain's Admin"
+        } )
+end
+
+Events:Subscribe("ModulesLoad", ModulesLoad)
+Events:Subscribe("ModuleUnload", ModuleUnload)
